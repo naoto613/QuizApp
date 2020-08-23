060a8f4d662f9502e27c454e029470ae0fca776c
naoto <you@example.com>
2020-08-24 08:22:30 +0900

    first commit

---
+++ app/src/main/java/com/example/pyonsu/quizapp/Question.kt
@@ -0,0 +1,12 @@
+package com.example.pyonsu.quizapp
+
+class Question (
+    val id: Int,
+    val question: String,
+    val image: Int,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
+    var correctAnswer: Int
+)
